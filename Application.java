be69afe668d18aeb2ffce1743480acedb9fa0036
cubeiplkumar <emailanilkumarm@gmail.com>
2023-11-01 19:04:23 +0530

    feature302 changes done

--- Application.java
+++ Application.java
@@ -89,6 +89,31 @@ public class ConsoleApplet extends java.applet.Applet
       add("South",temp);
       
    }
+#FEATURE302 changes by pritham on Registration Module
+ public void display1() {
+    System.out.println("Method without parameter");
+  }
+
+  // method with single parameter
+  public void display2(int a) {
+    System.out.println("Method with a single parameter: " + a);
+  }
+
+  public static void main(String[] args) {
+    
+    // create an object of Main
+    Main obj = new Main();
+
+    // calling method with no parameter
+    obj.display1();
+    
+    // calling method with the single parameter
+    obj.display2(24);
+  }
+}
+
+
+
 
    public Insets getInsets() {
       return new Insets(2,2,2,2);
